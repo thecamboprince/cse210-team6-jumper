24c4d09ce4785f7fcdb2b7333d949757429bebb0
AlexOKeeffe123 <oke20002@byui.edu>
2021-10-15 11:41:07 -0600

    Alex's part

--- jumper/game/director.py
+++ jumper/game/director.py
@@ -1,4 +1,16 @@
 
 
 class Director:
-    pass
+
+    def int(self):
+        pass
+
+    def jumper(self):
+        guess = input("Guess a letter [a-z]:").lower()
+        if guess in self.secret_word:
+            return False
+        else:
+            return True
+    #I chose to arange the true false because I thought that what ever jumper it goes to whether to cut the parachute or not. Feel free to change it
+    #I like the enhancement of the word selection from being an external file. there is a prepared file with a bunch of words.
+'''Compares to see if the user's input is in the string and returns either true or false'''
