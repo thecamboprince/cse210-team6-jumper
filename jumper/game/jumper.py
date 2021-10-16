6e1c2ab604cbdefdfdcc7318d9ddbd4568e48fc5
Tyson Freeze <tyson.freeze@icloud.com>
2021-10-16 11:37:13 -0700

    finished jumper

--- jumper/game/jumper.py
+++ jumper/game/jumper.py
@@ -1,18 +1,59 @@
+class Jumper:
+	"""A code template for person jumping with a parachute. The responsibility of 
+    this class of objects is to track the parachute and person's vitality.
 
-# Alex
-class Jumper: # Models the jumper guy. (tracks the state of the parachute and jumper.)
-	
+    
+    Attributes:
+        parachuteSize (number): The total number of "strands" on the chute.
+		parachuteStrands (number): The total number of "strands" left on the chute.
+    """
+
 	def __init__(self):
-		self.parachuteStrands = 4
+		"""Initializes the Jumper class object with a chute of size 4 and current strands 4.
+
+        Args:
+            self (Jumper): An instance of Jumper.
+        """
+		self.parachuteSize = 4
+		self.parachuteStrands = self.parachuteSize
 
 	def breakStrands(self):
-		pass
+		"""Breaks (subtracts) one of the strands.
 
-	def getNumStrands(self):
-		pass
+        Args:
+            self (Jumper): An instance of Jumper.
+        """
+		self.parachuteStrands -= 1
 
-	def getNumStrandsLeft(self):
-		pass
+	def getChuteSize(self):
+		"""Returns the number of strands the parachute started with
+
+        Args:
+            self (Jumper): An instance of Jumper.
+        
+        Returns:
+            number: Representing the count of strands at the start.
+        """
+		return self.parachuteSize
+
+	def getStrandsLeft(self):
+		"""Returns the number of strands still left
+
+        Args:
+            self (Jumper): An instance of Jumper.
+        
+        Returns:
+            number: Representing the count of strands left.
+        """
+		return self.parachuteStrands
 
 	def isAlive(self):
-		pass
+		"""Checks the number of strands left to see if he is freefalling (dead).
+
+        Args:
+            self (Jumper): An instance of Jumper.
+        
+        Returns:
+            boolean: Representing if the jumpMan has/will imminently die.
+        """
+		return self.getStrandsLeft() > 0
